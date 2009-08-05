811a0a1c31acf59a62333747cfb2820c8c42b4f5
Matthew Lang <matthew@matthewlang.co.uk>
2009-08-05 21:28:02 +0100

    Added class to create stats for updates and mentions.

---
+++ app/lib/statistic_builder.rb
@@ -0,0 +1,17 @@
+class StatisticBuilder < BaseBuilder
+  
+  def initialize(updates, count = 0)
+    super(updates, count)
+    build_sparkline_data
+  end
+  
+  def build_sparkline_data
+    @updates.each do |update|
+      day = (Date.today - Date.parse(update.created_at)).to_i
+      if day.between?(0,29)
+        @sparkline_data[day] += 1
+      end  
+    end
+  end
+  
+end
