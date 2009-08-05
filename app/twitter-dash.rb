04a3e2575d93e4b22880f88aebc285a5719b990b
Matthew Lang <matthew@matthewlang.co.uk>
2009-08-05 20:06:18 +0100

    Added routines to connect to Twitter.

--- app/twitter-dash.rb
+++ app/twitter-dash.rb
@@ -1,2 +1,48 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'twitter'
+
+def connect(username, password)
+  httpauth = Twitter::HTTPAuth.new(username,password)
+  Twitter::Base.new(httpauth)
+end
+
+def get_tweets
+  client = connect
+  tweets = client.user_timeline
+  page = 2
+  
+  while Time.parse(tweets.last.created_at) > 30.days.ago
+    tweets += client.user_timeline(:page => page)
+    page += 1
+  end
+  
+  tweets
+end
+
+def get_mentions
+  client = connect
+  mentions = client.replies
+  page = 2
+  
+  while Time.parse(mentions.last.created_at) > 30.days.ago
+    mentions += client.replies(:page => page)
+    page += 1
+  end
+  
+  mentions
+end
+
+get '/' do
+  haml :index
+end
+
+get '/login' do
+  haml :login
+end
+
+post 'login' do
+  title = params[:username]
+  link = params[:password]
+end
