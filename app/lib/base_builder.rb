8141ff0a469ba33f726dba38bc1b847ad12915fa
Matthew Lang <matthew@matthewlang.co.uk>
2009-08-05 21:26:09 +0100

    Added base class that all twitter stats will be based on.

---
+++ app/lib/base_builder.rb
@@ -0,0 +1,17 @@
+require 'gchart'
+
+class BaseBuilder
+  
+  attr_accessor :count
+    
+  def initialize(updates, count = 0)
+    @updates = updates
+    @count = count
+    @sparkline_data = Array.new(30) { |i| 0 }
+  end
+  
+  def sparkline
+    Gchart.sparkline(:data => @sparkline_data,:size => '80x20')
+  end
+  
+end
